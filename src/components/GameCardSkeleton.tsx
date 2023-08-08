91c43418620b1648772a0dd025ca4724c26807d7
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-08-08 12:55:30 +0100

    Showed loading (responsive) skeletons.

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,25 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+import React from "react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card
+      width={{
+        sm: "465px",
+        md: "350px",
+        lg: "280px",
+        xl: "240px",
+        "2xl": "450px",
+      }}
+      borderRadius={10}
+      overflow="hidden"
+    >
+      <Skeleton height="200px" />
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
